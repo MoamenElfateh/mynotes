fad5bddcc00339d4e5c4d2f346b900b3acffa269
Moamen Elfateh <moamenelfateh2@gmail.com>
2025-02-04 05:10:20 +0200

    step-16

--- lib/services/crud/notes_service.dart
+++ lib/services/crud/notes_service.dart
@@ -15,7 +15,9 @@ class NotesService {
   static final NotesService _shared = NotesService._sharedInstance();
   NotesService._sharedInstance() {
     _notesStreamController = StreamController<List<DatabaseNote>>.broadcast(
-      onListen: () {},
+      onListen: () {
+        _notesStreamController.sink.add(_notes);
+      },
     );
   }
   factory NotesService() => _shared;
