adaffc7ed6f0a267b7a4526769399815785df51c
Moamen Elfateh <moamenelfateh2@gmail.com>
2025-02-04 06:48:54 +0200

    step-18

---
+++ lib/extensions/list/filter.dart
@@ -0,0 +1,6 @@
+// function that allows us to filter a stream of list of something with whereclause
+
+extension Filter<T> on Stream<List<T>> {
+  Stream<List<T>> filter(bool Function(T) where) =>
+      map((items) => items.where(where).toList());
+}
--- lib/services/crud/notes_service.dart
+++ lib/services/crud/notes_service.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
+import 'package:mynotes/extensions/list/filter.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' show join;
@@ -10,6 +11,7 @@ class NotesService {
   Database? _db;
 
   List<DatabaseNote> _notes = [];
+  DatabaseUser? _user;
 
 // make NoteServixe a singleton
   static final NotesService _shared = NotesService._sharedInstance();
@@ -20,18 +22,36 @@ class NotesService {
       },
     );
   }
+
   factory NotesService() => _shared;
 
   late final StreamController<List<DatabaseNote>> _notesStreamController;
 
-  Stream<List<DatabaseNote>> get allNotes => _notesStreamController.stream;
+  Stream<List<DatabaseNote>> get allNotes =>
+      _notesStreamController.stream.filter((note) {
+        final currentUser = _user;
+        if (currentUser != null) {
+          return note.userId == currentUser.id;
+        } else {
+          throw UserShouldBeSetBeforeReadingAllNote();
+        }
+      });
 
-  Future<DatabaseUser> getOrCreateUser({required String email}) async {
+  Future<DatabaseUser> getOrCreateUser({
+    required String email,
+    bool setAsCurrentUser = true,
+  }) async {
     try {
       final user = await getUser(email: email);
+      if (setAsCurrentUser) {
+        _user = user;
+      }
       return user;
     } on CouldNotFindUser {
       final createdUser = await createUser(email: email);
+      if (setAsCurrentUser) {
+        _user = createdUser;
+      }
       return createdUser;
     } catch (e) {
       rethrow;
